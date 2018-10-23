cfe689db27c1059c4cede01a83ad8ce0517eb70b
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-10-23 00:51:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
   # code goes here
+  attr_accessor
+  @@all = []
+  def initialize
+    
+    @@all << self
+  end
+  
+  def self.all
+    return @@all
+  end
+  
+  def self.count
+    return @@all.length
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+    
 end
